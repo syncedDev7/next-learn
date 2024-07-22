075a79937be43709338c6e1e0a43e846b12e5a0e
Your Name <you@example.com>
2024-07-22 20:10:19 +0530

    chanes done before deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
+//font shiii
+import { lusitana } from '@/app/ui/fonts';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from "@/app/ui/home.module.css"
@@ -6,6 +8,7 @@ import styles from "@/app/ui/home.module.css"
 import Image from 'next/image';
 export default function Page() {
   return (
+    
     <main className="flex min-h-screen flex-col p-6">
       
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
@@ -15,7 +18,10 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
         <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"/>
         <div className={styles.shape}/>
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          
+    <p
+      className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+    >
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
